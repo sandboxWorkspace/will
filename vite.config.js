f93f5fbcd2a4d6c8fa8dfc894a7eaa6051d522a0
will-liu014 <25445820+will-liu014@users.noreply.github.com>
2025-04-02 17:27:07 -0500

    directory restructure

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath, URL } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: '/will/',
   publicDir: 'public',
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'), // Alias for root directory
-      '@ui': path.resolve(__dirname, 'ui'),
-      '@utils': path.resolve(__dirname, 'utils'),
-      '@data': path.resolve(__dirname, 'data'),
-      '@database': path.resolve(__dirname, 'database'),
+      '@ui': path.resolve(__dirname, './ui'),
+      '@utils': path.resolve(__dirname, './utils'),
+      '@data': path.resolve(__dirname, './data'),
+      '@database': path.resolve(__dirname, './database'),
+      '@src': path.resolve(__dirname, './src'),
+      '@css': path.resolve(__dirname, './src/css')
     },
   },
   build: {
